5dcc70d780272cafff8a54ffdd8c6d65b8efd25f
Adarsh <adarshpro43@mail.com>
2021-05-21 09:16:27 +0530

    After Making Login And Registration and redirecting 4 quad dashboard and It is Running

--- app/src/main/java/com/example/quiz/DashBoard.java
+++ app/src/main/java/com/example/quiz/DashBoard.java
@@ -1,30 +1,69 @@
 package com.example.quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class DashBoard extends AppCompatActivity {
-            TextView t1,t2,t3,t4;
+         private    TextView t1,t2,t3,t4;
+        private     CardView t11,t12,t13,t14;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+      //  Log.d("checkAct", "onCreate() called with: savedInstanceState = [" + savedInstanceState + "]"+TAG);
         t1=findViewById(R.id.tktest);
+        t11=findViewById(R.id.tktestcard);
         t2=findViewById(R.id.mktest);
-        t2=findViewById(R.id.wtfriends);
-        t3=findViewById(R.id.review);
+        t12=findViewById(R.id.mktestcard);
+        t3=findViewById(R.id.wtfriends);
+        t13=findViewById(R.id.wtfriendscard);
+        t4=findViewById(R.id.review);
+        t14=findViewById(R.id.reviewcard);
         t1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               // t1.setBackgroundColor(112233);
+                t11.setBackgroundColor(R.drawable.option_border);
                 Intent intent = new Intent(DashBoard.this,
                         QuestionActivity.class);
                 startActivity(intent);
                // finish();
+            }
+        });
+        t2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                t12.setBackgroundColor(R.drawable.option_border);
+//                Intent intent = new Intent(DashBoard.this,
+//                        QuestionActivity.class);
+//                startActivity(intent);
+                // finish();
+            }
+        });
+        t3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                t13.setBackgroundColor(R.drawable.option_border);
+//                Intent intent = new Intent(DashBoard.this,
+//                        QuestionActivity.class);
+//                startActivity(intent);
+                // finish();
+            }
+        });
+        t4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                t14.setBackgroundColor(R.drawable.option_border);
+//                Intent intent = new Intent(DashBoard.this,
+//                        QuestionActivity.class);
+//                startActivity(intent);
+                // finish();
             }
         });
 
--- app/src/main/java/com/example/quiz/Login1.java
+++ app/src/main/java/com/example/quiz/Login1.java
@@ -1,21 +1,25 @@
 package com.example.quiz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Login1 extends AppCompatActivity {
     private FirebaseAuth fire;
+  //  private static final String TAG = "Login1";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,12 +51,14 @@ public class Login1 extends AppCompatActivity {
         fire.signInWithEmailAndPassword(email.getText().toString(),pass1et.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
             @Override
             public void onSuccess(AuthResult authResult) {
+              //  Log.d("checkAct", "Login onSuccess() called with: authResult = [" + authResult + "]"+TAG);
                 Toast.makeText(Login1.this, "Successfully Login", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(Login1.this,
                                 DashBoard.class);
                         startActivity(intent);
                         finish();
             }
+
         });
     }
 }
